4a872d1cc95536ed0452c81261cdaccd40ca8e01
Ayomikun <elijahsholademi@gmail.com>
2022-03-26 13:18:54 +0100

    fixed production issues

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,40 @@
-import { fileURLToPath, URL } from 'url'
+// import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//       process: "process/browser",
+//       stream: "stream-browserify",
+//       zlib: "browserify-zlib",
+//       util: "util",
+//       buffer: "buffer",
+//     },
+//   },
+// });
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
       process: "process/browser",
       stream: "stream-browserify",
       zlib: "browserify-zlib",
-      util: 'util'
-    }
-  }
-})
+      util: "util",
+      web3: path.resolve(__dirname, "./node_modules/web3/dist/web3.min.js"),
+    },
+  },
+});
